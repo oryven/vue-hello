9ce858a87b4887c0300c0b768bdc44670bb30e33
Orazio <orazioventrella@hotmail.it>
2021-11-10 15:31:17 +0100

    inserito titolo

---
+++ js/main.js
@@ -0,0 +1,16 @@
+/* 
+Descrizione:
+Stampare a schermo un messaggio all’interno di un h1,
+utilizzando i data.
+Bonus:
+Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+*/
+
+var app = new Vue (
+    {
+        el: "#container",
+        data: {
+            message: "Hello Vue!",
+        }
+    }
+);
